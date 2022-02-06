2cfaa9e4f6a8d731651831a65ec320630337c8c5
Edgar <67913087+Metaphysicist1@users.noreply.github.com>
2022-02-06 17:43:46 +0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,28 @@
+sudo apt install git virtualenv
+
+git clone https://github.com/Metaphysicist1/application
+
+function act(){
+	virtualenv .speech
+
+	source .speech/bin/activate
+	cd application
+}
+
+act
+
+function other(){
+	pip install -r requirements.txt
+
+	sudo apt-get install  python3-all-dev python3-pip build-essential swig  libpulse-dev libasound2-dev
+
+	gdown https://drive.google.com/uc?id=1rMV2hrefpMy7C8ShF4QsNc61J4VzRurn
+	unzip daanzu_vosk.zip
+
+	gdown https://drive.google.com/uc?id=1II3B_m-FEniKikCEJrTd_sAURfeDmLGr
+	unzip small_vosk.zip
+
+	pip install pocketsphinx==0.1.15
+	
+	}
+other
